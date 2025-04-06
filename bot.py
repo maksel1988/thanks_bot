bbaae18360d6abd17096654b683607888ba2b3d8
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-06 03:16:46 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ ADMIN_ID = 49952799
 # Настройки PostgreSQL
 DB_NAME = os.getenv('DB_NAME', 'thanks_bot')
 DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', '')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'Qwerty123456')
 DB_HOST = os.getenv('DB_HOST', 'localhost')
 DB_PORT = os.getenv('DB_PORT', '5432')
 
