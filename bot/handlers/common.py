70fd5aa7a7e947fc386695161f79a0c96525be64
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-09 00:50:57 +0200

    Update common.py

--- bot/handlers/common.py
+++ bot/handlers/common.py
@@ -1,1 +1,14 @@
+from aiogram import types
+from config import Config
 
+async def cmd_start(message: types.Message):
+    welcome_text = """
+    Привет! Я бот для отправки благодарностей коллегам.
+
+    Чтобы отправить благодарность, напиши сообщение в формате:
+    @username1 @username2 Текст вашей благодарности...
+
+    Пример:
+    @ivan @anna Спасибо за помощь с проектом! Вы молодцы!
+    """
+    await message.answer(welcome_text)
