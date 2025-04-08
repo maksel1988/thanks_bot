585e300bef8b375246679fe496bd724c55164ba3
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-09 00:45:16 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,32 @@
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from config import Config
+from .handlers import common, thanks
 
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=Config.BOT_TOKEN)
+storage = MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
+
+async def on_startup(dp):
+    await bot.send_message(Config.ADMIN_ID, "Бот запущен")
+
+async def on_shutdown(dp):
+    await bot.send_message(Config.ADMIN_ID, "Бот остановлен")
+    await dp.storage.close()
+    await dp.storage.wait_closed()
+
+def setup_handlers():
+    dp.register_message_handler(common.cmd_start, commands=["start"])
+    dp.register_message_handler(thanks.process_thanks_message, content_types=types.ContentType.TEXT)
+
+def main():
+    setup_handlers()
+    
+    from aiogram import executor
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
+
+if __name__ == '__main__':
+    main()
