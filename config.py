7f2844b92a00f42c278e373ee88c4a8840264597
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-09 00:43:29 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    ADMIN_ID = int(os.getenv('ADMIN_ID'))
+    CHANNEL_ID = os.getenv('CHANNEL_ID')
+    
+    DB_HOST = os.getenv('DB_HOST')
+    DB_PORT = os.getenv('DB_PORT')
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    
+    WEB_HOST = os.getenv('WEB_HOST', '0.0.0.0')
+    WEB_PORT = int(os.getenv('WEB_PORT', 5000))
