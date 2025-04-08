09a2e72958a07277d6bd832dd6f9742fa3c8f116
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-09 00:55:27 +0200

    Update __init__.py

--- web/__init__.py
+++ web/__init__.py
@@ -1,1 +1,9 @@
+from flask import Flask
+from bot.services.db import Base, engine
 
+app = Flask(__name__)
+
+# Инициализация базы данных
+Base.metadata.create_all(bind=engine)
+
+from . import views
