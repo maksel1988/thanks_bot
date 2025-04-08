b66f9a85c65d1c279b4899368715ae177d594016
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-09 00:44:17 +0200

    Update thanks.py

--- bot/models/thanks.py
+++ bot/models/thanks.py
@@ -1,1 +1,15 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from ..services.db import Base
 
+class ThanksMessage(Base):
+    __tablename__ = 'thanks_messages'
+    
+    id = Column(Integer, primary_key=True)
+    sender_id = Column(Integer)
+    sender_username = Column(String)
+    receiver_usernames = Column(String)  # храним как строку "@user1 @user2"
+    message_text = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+    
+    def __repr__(self):
+        return f"<ThanksMessage(from={self.sender_username}, to={self.receiver_usernames})>"
