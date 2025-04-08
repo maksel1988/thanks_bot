a348cf7bdb25aa243eb125645a13aa74d6132e81
maksel1988 <96349225+maksel1988@users.noreply.github.com>
2025-04-09 00:55:10 +0200

    Update helpers.py

--- bot/utils/helpers.py
+++ bot/utils/helpers.py
@@ -1,1 +1,10 @@
+import re
 
+def parse_usernames(text: str):
+    """Извлекает упоминания пользователей из текста"""
+    usernames = re.findall(r'@\w+', text)
+    return usernames
+
+def format_thanks_message(sender: str, receivers: list, message: str):
+    """Форматирует сообщение благодарности для канала"""
+    return f"{' '.join(receivers)}\n\n{message}\n\nОт: @{sender}"
